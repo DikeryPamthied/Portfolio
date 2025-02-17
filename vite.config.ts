cc669ced9133fc5544079baf4780826a7d68226a
DikeryPamthied <dikery377@gmail.com>
2025-02-17 15:18:50 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: '/Portfolio/',  // Ensure leading and trailing slash
   build: {
+    assetsInlineLimit: 0,
     sourcemap: true,
     chunkSizeWarningLimit: 1600,
   },
